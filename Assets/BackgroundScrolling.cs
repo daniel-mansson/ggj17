2ded14aa8e597cd685851052c7ab040641f1c7ec
Daniel <daniel.maansson@gmail.com>
2017-01-20 22:15:40 +0100

    Added background and some fishy stuff

---
+++ Assets/BackgroundScrolling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+	public float m_speed = 0;
+	public Material m_material;
+
+	MeshRenderer m_renderer;
+	Material m_materialClone;
+	// Use this for initialization
+	void Start () {
+		m_materialClone = new Material(m_material);
+		m_renderer = GetComponent<MeshRenderer>();
+		m_renderer.material = m_materialClone;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_materialClone.mainTextureOffset += Vector2.right * Time.deltaTime * m_speed;
+		m_renderer.material = m_materialClone;
+	}
+}
--- Assets/Scripts/WhaleFish.cs
+++ Assets/Scripts/WhaleFish.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WhaleFish : MonoBehaviour {
+public class WhaleFish : MonoBehaviour
+{
+	public float m_force;
+	public int m_playerId = 0;
+	public float m_rotScale = 0f;
 
-	// Use this for initialization
-	void Start () {
-		
+	Rigidbody2D m_body;
+	Controller m_controller;
+
+	void Start ()
+	{
+		m_controller = Systems.Instance.Input.GetController(m_playerId);
+		m_body = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate ()
+	{
+		var joy = m_controller.GetJoystick(Xbox360ControllerJoystickId.Left);
+		m_body.AddForce(m_force * joy);
+
+		m_body.rotation = m_body.velocity.y * m_rotScale;
 	}
 }
