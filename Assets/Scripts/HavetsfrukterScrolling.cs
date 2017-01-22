04ab96b1f30e1a55d541e7902b457304d6f09754
Bill Kurtson <Bill Kurtson>
2017-01-22 13:17:08 +0100

    add speed magnifier to foreground parallax dependent on no of foods eaten

--- Assets/Scripts/HavetsfrukterScrolling.cs
+++ Assets/Scripts/HavetsfrukterScrolling.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class HavetsfrukterScrolling : MonoBehaviour {
     private float speed = 0.04f;
     private float maxLeft;
+    private float speedMagnifier;
+    private int lastEat;
 
 	// Use this for initialization
 	void Start () {
         maxLeft = -70.0f;
+        speedMagnifier = 1.02f;
+        lastEat = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         var movement = new Vector3(-1.0f, 0, 0);
+        var noOfFoodsEaten = EatCounter.NFoodsDestroyed();
+
+        if (noOfFoodsEaten != lastEat)
+        {
+            lastEat = noOfFoodsEaten;
+            speed *= Mathf.Pow(speedMagnifier, noOfFoodsEaten);
+        }
 
         transform.Translate(movement * speed);
 
